df8002890d301a9535a65ea39e764bf515739af2
wevexOP <111082615+wevexOP@users.noreply.github.com>
2023-02-20 15:21:07 -0800

    updated seed data and changed some of the homepage layout

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -2,45 +2,45 @@ const { User } = require('../models');
 
 const userData = [
     {
-        username: "martin_bour",
-        twitter: "martinb",
-        github: "martinb",
-        email: "martin_b@gmail.com",
+        username: "bot4",
+        twitter: "bot4",
+        github: "bot4",
+        email: "email4@email.com",
         password: "p@ssword1"
     },
     {
-        username: "matt_b",
-        twitter: "mathewb",
-        github: "mathewb",
-        email: "mathew_b@gmail.com",
+        username: "bot5",
+        twitter: "bot5",
+        github: "bot5",
+        email: "email5@email.com",
         password: "p@ssword2"
     },
     {
-        username: "shaun_c",
-        twitter: "shaun",
-        github: "shaun",
-        email: "shaun_c@gmail.com",
+        username: "bot6",
+        twitter: "bot6",
+        github: "bot6",
+        email: "email6@email.com",
         password: "p@ssword3"
     },
     {
-        username: "lee_n",
-        twitter: "leenorris",
-        github: "leenorris",
-        email: "lee_n@gmail.com",
+        username: "bot3",
+        twitter: "bot3",
+        github: "bot3",
+        email: "email3@email.com",
         password: "p@ssword4"
     },
     {
-        username: "priya_r",
-        twitter: "priyaravi23",
-        github: "priyaravi23",
-        email: "priya_r@gmail.com",
+        username: "bot1",
+        twitter: "bot1",
+        github: "bot1",
+        email: "email1@email.com",
         password: "p@ssword5"
     },
     {
-        username: "pooja",
-        twitter: "pooja_w",
-        github: "pooja",
-        email: "pooja@gmail.com",
+        username: "bot2",
+        twitter: "bot2",
+        github: "bot2",
+        email: "email2@email.com",
         password: "p@ssword6"
     }
 ]
